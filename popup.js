cdea35df2de417c3a5e1499450b9ed0beed1eea3
pandovic <wisdompandovic@gmail.com>
2023-10-02 22:16:12 +0100

    added booking form

---
+++ popup.js
@@ -0,0 +1,9 @@
+function showForm() {
+    document.getElementById('popup-form').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeForm() {
+    document.getElementById('popup-form').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
